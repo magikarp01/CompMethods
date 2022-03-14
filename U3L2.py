9dbbb20bb1cfe7affd3204836d83c7e76e0d7451
Phillip Guo <philliphguo@gmail.com>
2022-03-14 10:57:40 -0400

    Progress on U3L2

--- U3L2.py
+++ U3L2.py
@@ -6,6 +6,7 @@ import math
 def approx_error(x_i, x_f):
     return abs((x_f-x_i)/x_f)
 
+
 def fixed_point(g, tol, x_0):
     print("Fixed Point Iteration")
     x_f = g(x_0)
@@ -23,7 +24,7 @@ def fixed_point(g, tol, x_0):
         print(f"Approximate Error is {approx_error(x_0, x_f)}")
         print()
 
-def newton_raphson(f, deriv, tol, x_0):
+def newton_raphson(f, deriv, tol, x_0, iters=-1):
     print("Newton-Raphson Method")
     x_f = x_0 - f(x_0)/deriv(x_0)
     iter = 0
@@ -32,14 +33,85 @@ def newton_raphson(f, deriv, tol, x_0):
     print(f"Approximate Error is {approx_error(x_0, x_f)}")
     print()
 
-    while approx_error(x_0, x_f) > tol:
-        iter+=1
-        x_0 = x_f
-        x_f = x_0 - f(x_0)/deriv(x_0)
-        print(f"Iteration {iter}")
-        print(f"New x is {x_f}")
-        print(f"Approximate Error is {approx_error(x_0, x_f)}")
-        print()
+    if iters==-1:
+        while approx_error(x_0, x_f) > tol:
+            iter+=1
+            x_0 = x_f
+            x_f = x_0 - f(x_0)/deriv(x_0)
+            print(f"Iteration {iter}")
+            print(f"New x is {x_f}")
+            print(f"Approximate Error is {approx_error(x_0, x_f)}")
+            print()
+
+    else:
+        for i in range(iters):
+            iter += 1
+            x_0 = x_f
+            x_f = x_0 - f(x_0) / deriv(x_0)
+            print(f"Iteration {iter}")
+            print(f"New x is {x_f}")
+            print(f"Approximate Error is {approx_error(x_0, x_f)}")
+            print()
+
+def secant_method(f, tol, x_0, x_1, iters=-1):
+    print("Secant Method")
+    x_2 = f(x_1)*(x_0-x_1)/(f(x_0) - f(x_1))
+    iter = 0
+    print(f"Iteration {iter}")
+    print(f"New x is {x_2}")
+    print(f"Approximate Error is {approx_error(x_1, x_2)}")
+    print()
+    if iter == -1:
+        while approx_error(x_1, x_2) > tol:
+            iter += 1
+            x_0 = x_1
+            x_1 = x_2
+            x_2 = f(x_1)*(x_0-x_1)/(f(x_0) - f(x_1))
+            print(f"Iteration {iter}")
+            print(f"New x is {x_2}")
+            print(f"Approximate Error is {approx_error(x_1, x_2)}")
+            print()
+
+    else:
+        for i in range(iters):
+            iter += 1
+            x_0 = x_1
+            x_1 = x_2
+            x_2 = f(x_1) * (x_0 - x_1) / (f(x_0) - f(x_1))
+            print(f"Iteration {iter}")
+            print(f"New x is {x_2}")
+            print(f"Approximate Error is {approx_error(x_1, x_2)}")
+            print()
+
+def modified_secant(f, tol, delta, x_0, iters=-1):
+    print("Newton-Raphson Method")
+    x_f = x_0 - delta * x_0 * f(x_0) / (f(x_0 + delta*x_0) - f(x_0))
+    iter = 0
+    print(f"Iteration {iter}")
+    print(f"New x is {x_f}")
+    print(f"Approximate Error is {approx_error(x_0, x_f)}")
+    print()
+
+    if iters == -1:
+        while approx_error(x_0, x_f) > tol:
+            iter += 1
+            x_0 = x_f
+            x_f = x_f = x_0 - delta * x_0 * f(x_0) / (f(x_0 + delta*x_0) - f(x_0))
+            print(f"Iteration {iter}")
+            print(f"New x is {x_f}")
+            print(f"Approximate Error is {approx_error(x_0, x_f)}")
+            print()
+
+    else:
+        for i in range(iters):
+            iter += 1
+            x_0 = x_f
+            x_f = x_f = x_0 - delta * x_0 * f(x_0) / (f(x_0 + delta * x_0) - f(x_0))
+            print(f"Iteration {iter}")
+            print(f"New x is {x_f}")
+            print(f"Approximate Error is {approx_error(x_0, x_f)}")
+            print()
+
 
 
 print("Problem 6.1")
@@ -55,3 +127,7 @@ fixed_point(g, .0001, 5)
 print("Part B:")
 newton_raphson(f, deriv, .0001, 5)
 
+print("Problem 6.3")
+f = lambda x: x**3 - 6*x*x + 11*x - 6.1
+print("Graphically: by Desmos, 3.047")
+print("Part B: ")
